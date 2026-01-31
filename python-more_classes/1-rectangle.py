9848dc4fa3ea537548a022c48ff3d49abc37d471
your0T0 <yara241020@gmail.com>
2026-01-31 12:46:15 +0000

    add

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,10 +11,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """Get the width."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,12 +25,14 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = valu
+        self.__height = value
