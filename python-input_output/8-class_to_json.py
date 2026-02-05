924d9f0fc65366bfc7d648cbf0aafeac47e808a5
your0T0 <yara241020@gmail.com>
2026-02-05 15:08:41 +0000

    add all

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Class to JSON (dict description)."""
+"""Class to JSON."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description of obj."""
-    return obj.__dic__
+    """Return dict description of obj."""
+    return obj.__dict__
