5d8a2ae1030f3190b6397270e9fa0b20962ed1e9
your0T0 <yara241020@gmail.com>
2026-02-28 12:18:10 +0000

    Fix alias with backticks

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,4 @@
 -- Lists all databases of the MySQL server sorted alphabetically
-SELECT schema_name AS Database
+SELECT schema_name AS `Database`
 FROM information_schema.schemata
 ORDER BY schema_name;
